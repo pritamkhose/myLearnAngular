eec0cfe8b6809777c2bc6329de46e74f9dfb3591
Pritam Khose <pritamkhose@users.noreply.github.com>
2018-03-31 13:20:00 +0530

    V1.0.3 : Working with Component Styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // styleUrls: ['./servers.component.css']
+  styles: ['h3{color: red}']
 })
 export class ServersComponent implements OnInit {
 
