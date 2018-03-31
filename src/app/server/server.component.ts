4072543a0ec58a7dafdc222b0f7ce93517ac2af1
Pritam Khose <pritamkhose@users.noreply.github.com>
2018-03-31 13:04:32 +0530

    V1.0.2 : Add template manually

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,16 @@
 ///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  // templateUrl: './server.component.html',
+  template: '<app-server></app-server><app-servers></app-servers>',
+  styleUrls: ['./server.component.css']
   }
 )
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
 }
