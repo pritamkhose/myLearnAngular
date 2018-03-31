fa361b8578cdbf5f7db3d9555062ebd08dac9105
Pritam Khose <pritamkhose@users.noreply.github.com>
2018-03-31 18:38:24 +0530

    V1.0.4 : String Interpolation and Data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  // styleUrls: ['./servers.component.css']
-  styles: ['h3{color: red}']
+   styleUrls: ['./servers.component.css']
+  // styles: ['h3{color: red}']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout( () => {
+      this.allowNewServer = true;
+    } , 2000 );
+  }
 
   ngOnInit() {
   }
