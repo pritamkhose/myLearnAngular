14b8a3c4c071e1e01fc548461d02d232374587b6
Pritam Khose <pritamkhose@users.noreply.github.com>
2018-03-31 12:38:13 +0530

    V1.0.2 : Add Server Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+  }
+)
+export class ServerComponent {
+}
